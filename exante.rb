7c0adb9eb0865a66a499182aff2de1dba6c8f734
Arunan Skanthan <arunan.skanthan@gmail.com>
2011-03-28 00:48:15 +1100

    Added sqlite db and datamapper code

--- exante.rb
+++ exante.rb
@@ -1,6 +1,31 @@
 require 'rubygems'
 require 'sinatra'
+require 'dm-core'
+require 'dm-migrations'
+require 'dm-timestamps'
 
+# gem install sqlite do_sqlite3 datamapper
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/db/exante.db")
+
+class Event
+	
+	include DataMapper::Resource
+	
+	property :id,				Serial
+	property :title,			String
+	property :description,		Text
+	property :start_date,		DateTime
+	property :start_time,		DateTime
+	property :end_date,			DateTime
+	property :end_time,			DateTime
+	property :all_day,			Boolean
+	property :location,			Text
+	property :guest_list,		String
+	property :private_event,	Boolean
+	
+end
+
+DataMapper.auto_upgrade!
 
 get '/' do
 	@title = "Welcome"
