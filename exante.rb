4fb44ce1b71b1f2bb8326d573422ee076d17582d
Buddhika Siddhisena <bud@thinkcube.com>
2011-03-28 15:49:19 +0530

    Basic implementation of create event.

--- exante.rb
+++ exante.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
+require 'date'
 
 # gem install sqlite do_sqlite3 datamapper
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db/exante.db")
@@ -30,9 +31,43 @@ get '/' do
 	erb :home
 end
 
+# New event form
 get '/event/new' do
 	@title = "Create new event"
 	erb :new_event
+end
+
+# Create event
+post '/event/create' do
+	#params.inspect
+	event = Event.new
+	event.title = params[:title]
+	event.description = params[:description]
+	event.location = params[:location]
+	event.guest_list = params[:guest_list]
+
+	start_date = params[:start_date]
+	event.start_date = Date.strptime(start_date, "%Y-%m-%d")
+
+	start_time = params[:start_time]
+	event.start_time = DateTime.strptime("#{start_date} #{start_time}", "%Y-%m-%d %H:%M")
+
+	end_date = params[:end_date]
+	event.end_date = Date.strptime(end_date, "%Y-%m-%d")
+
+	end_time = params[:end_time]
+	event.end_time = DateTime.strptime("#{end_date} #{end_time}", "%Y-%m-%d %H:%M")
+
+	# TODO: Handle All day events
+
+	if event.save
+		status 201
+		redirect '/event/' + event.id.to_s
+	else
+		status 412
+		redirect '/events'
+	end
+
 end
 
 post '/list' do
