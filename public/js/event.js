bfac568179e46d7bb532482a0bfcdb3734a1e5a9
Buddhika Siddhisena <bud@thinkcube.com>
2011-03-29 16:44:05 +0530

    Added jquery-ui

--- public/js/event.js
+++ public/js/event.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
- $('#all-day').click(function() {
-  $('#to-date-time').toggle();
+
+	// Date picker
+	$( "#start_date" ).datepicker( "option", "dateFormat", "yy-mm-dd");
+
+ // All day toggle
+ $('#all_day').click(function() {
+  // Don't show time
+  $("label[for='start_time']").toggle();
+  $('#start_time').toggle();
+
+  $("label[for='end_time']").toggle();
+  $('#end_time').toggle();
  });
 });
